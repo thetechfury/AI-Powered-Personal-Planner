22d06b97a5a208fd1842dc4873c0c419f4ed4877
Moeez Ali <moeez4@gmail.com>
2024-10-08 14:57:26 +0500

    formatting of frontend modified.

--- frontend/src/components/ui/calendar.js
+++ frontend/src/components/ui/calendar.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {DateCalendar} from '@mui/x-date-pickers/DateCalendar';
 
 export default function Calendar() {
-  return (
-      <div className="p-4 space-y-6">
-          <div className="bg-gray-800 rounded-lg p-4">
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DateCalendar slotProps={{ calendarHeader: { sx: { color: '#22d3ee',width:'100%' } } }}/>
-              </LocalizationProvider>
-          </div>
-      </div>
-          );
-          }
+    return (
+        <div className="p-4 space-y-6">
+            <div className="bg-gray-800 rounded-lg p-4">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DateCalendar slotProps={{calendarHeader: {sx: {color: '#22d3ee', width: '100%'}}}}/>
+                </LocalizationProvider>
+            </div>
+        </div>
+    );
+}
