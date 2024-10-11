cde1631e07e1302ab732004e8a80b03a4d1bcf4a
Moeez Ali <moeez4@gmail.com>
2024-10-11 17:09:38 +0500

    create context for api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import EventsPage from "./pages/EventsPage";
 import Footer from "./components/footer/Footer";
+import {ApiProvider} from "./ApiContext";
 
 function App() {
     return (
-        <div className="flex flex-col h-screen bg-gray-900 text-gray-100">
-            <Navbar/>
-            <EventsPage/>
-            <Footer/>
-        </div>
+        <ApiProvider>
+            <div className="flex flex-col h-screen bg-gray-900 text-gray-100">
+                <Navbar/>
+                <EventsPage/>
+                <Footer/>
+            </div>
+        </ApiProvider>
     );
 };
+
 export default App;
