e9bf4a5d8a6d14b0d290430adb8edb6af34a3cef
junaidttf <mjunaidttf@gmail.com>
2024-10-17 11:24:17 +0500

    get session id from cookie

--- backend/event/views.py
+++ backend/event/views.py
@@ -10,12 +10,26 @@ from event.models import CustomUser, Task, Category, Chat
 from event.serializers import CustomUserSerializer, TaskSerializer, CategorySerializer, ChatSerializer
 
 
+def get_session_id(request):
+    http_cookie = request.META.get('HTTP_COOKIE', '')
+    session_id = None
+    cookies = http_cookie.split(';')
+    for cookie in cookies:
+        key_value = cookie.strip().split('=')
+        if len(key_value) == 2:
+            key, value = key_value
+            if key == 'session_id':
+                session_id = value
+                break
+    return session_id
+
+
 class CustomUserViewSet(GenericAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
     def get(self, request):
-        session_id = self.get_session_id(request)
+        session_id = get_session_id(request)
         if session_id:
             user = CustomUser.objects.filter(session_id=session_id).first()
             if not user:
@@ -24,19 +38,6 @@ class CustomUserViewSet(GenericAPIView):
             user = self.create_user()
         serializer = CustomUserSerializer(user)
         return Response({"data": serializer.data}, status=status.HTTP_200_OK)
-
-    def get_session_id(self, request):
-        http_cookie = request.META.get('HTTP_COOKIE', '')
-        session_id = None
-        cookies = http_cookie.split(';')
-        for cookie in cookies:
-            key_value = cookie.strip().split('=')
-            if len(key_value) == 2:
-                key, value = key_value
-                if key == 'session_id':
-                    session_id = value
-                    break
-        return session_id
 
     def create_user(self):
         session_id = str(uuid.uuid4())
@@ -60,11 +61,11 @@ class ChatViewSet(GenericAPIView):
 
     def post(self, request):
         text = request.data.get('input_text', '')
-        user_session = request.data.get('session_id', '')
+        session_id = get_session_id(request)
 
-        if text and user_session:
+        if text and session_id:
             try:
-                user = CustomUser.objects.get(session_id=user_session)
+                user = CustomUser.objects.get(session_id=session_id)
                 Chat.objects.create(text=text, send_by='user', user=user)
             except CustomUser.DoesNotExist:
                 return Response({"error": "Invalid user session"}, status=status.HTTP_400_BAD_REQUEST)
@@ -74,8 +75,10 @@ class ChatViewSet(GenericAPIView):
         message_to_send = "helloworld"
         return Response({"message": message_to_send}, status=status.HTTP_200_OK)
 
+
 class Pagination(PageNumberPagination):
     page_size_query_param = 'page'
+
 
 class ChatListViewSet(ListAPIView):
     queryset = Chat.objects.all().order_by('-created_at')
