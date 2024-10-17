f1cb3bdc489b308afd847dbb62802bdb4d4b8135
junaidttf <mjunaidttf@gmail.com>
2024-10-17 15:09:23 +0500

    rename url

--- backend/event/urls.py
+++ backend/event/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('custom-user', views.CustomUserViewSet.as_view(), name='custom_user'),
     path('create-chat', views.ChatViewSet.as_view(), name='create_chat'),
-    path('chats', views.ChatListViewSet.as_view(), name='chats'),
+    path('chat-history', views.ChatListViewSet.as_view(), name='chats'),
 ]
